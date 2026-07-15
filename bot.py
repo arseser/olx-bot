850ad0048210209b84ddbc0634ceaddbd3b7f005
arseser <arseser8@gmail.com>
2026-07-15 20:46:37 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import time
 import telebot
 from telebot import types
 
-TOKEN = "8545685626:AAGmTF2wupXMxQ773lz0a4vjNUa4-KkXazQ"
+TOKEN = "8921710608:AAHyvGTupv9uutrfKf4lWCblA-pUKFLXm8I"
 bot = telebot.TeleBot(TOKEN)
 
 DOMAINS = ["@wp.pl", "@vp.pl"]
